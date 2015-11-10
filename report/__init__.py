d5b843a6b607133d2c1d96fd353a7851ed51f764
zurez <zurez4u@gmail.com>
2015-11-10 13:14:50 +0530

    cleaning

--- report/__init__.py
+++ report/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from flask.ext.mongoengine import MongoEngine
+
 # from flask.ext.triangle import Triangle
 
 app = Flask(__name__)
 
 from report import views
-app.config["MONGODB_SETTINGS"] = {'DB': "survey-test"}
-app.config["SECRET_KEY"] = "Some Secret Key"
-
-db=MongoEngine(app)
