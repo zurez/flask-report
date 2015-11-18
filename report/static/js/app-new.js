ebdc4e68c51297843f1d083a8a3dc32009fdd503
zurez <zurez4u@gmail.com>
2015-11-19 01:08:37 +0530

    Progress

--- report/static/js/app-new.js
+++ report/static/js/app-new.js
@@ -2,14 +2,14 @@ var myApp= angular.module("ReportApp",["chart.js"]);
 myApp.controller("ReportController",["$http","$scope","$location",function($http,$scope,$location){
 
 	  //Helper function 1
-    function check_if_exists (arg, aList) {
-        for (var i = 0; i < aList.length; i++) {
-            if (aList[i]==arg){
-                return true;
-            }
-            return false;
-        };
-    }//func1
+    // function check_if_exists (arg, aList) {
+    //     for (var i = 0; i < aList.length; i++) {
+    //         if (aList[i]==arg){
+    //             return true;
+    //         }
+    //         return false;
+    //     };
+    // }//func1
     // Helper function 2
     function cot (arr) {
         var a = [], b = [], prev;
@@ -42,10 +42,14 @@ myApp.controller("ReportController",["$http","$scope","$location",function($http
     			$scope.stitle= struct.game_title; //Set Survey Title
     			$scope.question_list= resp.columns.slice(2);//Get question List
     			$scope.total_question= $scope.question_list.length;
-    			console.log($scope.question_list);
+    			
     			
     			$scope.type="Text";
     			$scope.counter= 0;
+
+    			//We need to put up an init code here :(
+    				//So that the user doesn't sees an empty page.
+    				//
     			//Navigation Button Control
     			$scope.navigate= function(type){
     					if (type=="next") {$scope.counter+=1;}
@@ -67,7 +71,7 @@ myApp.controller("ReportController",["$http","$scope","$location",function($http
 
     						};
     					};
-    					console.log($scope.question_options);
+    			
     					//Get total responses for that question
     					var responses_for_a_cid= [];
     					var index= resp.columns.indexOf($scope.cid);
@@ -77,8 +81,61 @@ myApp.controller("ReportController",["$http","$scope","$location",function($http
     					};
     					// console.log(cot(["a","a","b","b"])); //Why wrong values? No idea.
     					//If an option has no values. then add 0 as its default value otherwise Graph will not initialize. Bleh!
-    					for (var i = 0; i < responses_for_a_cid.length; i++) {
-    						//
+    					var count = cot(responses_for_a_cid); //an array of two array [[options],[count]] // Remove null from here,
+    					var count_options = count[0];//Should have used the same variable .. ??? SHould learn DRY
+    					var count_options_total= count[1];
+    					function options_map(){
+    						var a={};
+    						for (var i = 1; i < $scope.question_options.length+1; i++) {
+    							// a.push("a_"+i.toString());
+    							a["a_"+i.toString()]=$scope.question_options[i-1];
+    							//{"a_1":"option"}
+    						};
+    						return a
+
+    					};
+    					var options_map= options_map(); //Useful for getting labels
+    					
+    					//check if all options are counted
+    					//Big mistake count[0]==[a_1, a_2] but question_options = [text ,]
+    					if (count[0].length!= $scope.question_options.length) {
+    						//This means some options have not been counted,
+    						//Find out which options have not been counted.
+    						var notcounted = $scope.question_options.filter(function(val) {
+												  return count[0].indexOf(val) == -1;
+												});
+    						//Not count is an array of options not been responded by any user.
+    						for (var i = 0; i < notcounted.length; i++) {
+    							count_options.push(notcounted[i]);
+    							count_options_total.push(0);
+    						};
+    						
+    					}
+    					//Some graph variables
+    					var type , label, series;
+    					//Now Set the Graph
+    					 if ($scope.question_type=="short_text") {
+    					 	$scope.right_title= "Responses";
+    					 	//write a function to get responses.
+    					 }
+    					 else if ($scope.question_type=="single_choice"){
+    					 		$scope.type="Pie";
+    					 		$scope.data=count_options_total;
+    					 		var labels=[];
+    					 		for (var i = 0; i < count_options.length; i++) {
+    					 			labels.push(count_options[i]);
+    					 		};
+    					 		$scope.label= count_options;
+    					 }
+    					 else if ($scope.question_type=="multiple_choice"){}
+    					 else if ($scope.question_type=="ranking"){}
+    					 else if ($scope.question_type=="rating"){}
+    					 else if ($scope.question_type=="yes_no"){};
+
+
+    					for (var i = 0; i < count[0].length; i++) {
+    						
+    						
     					};
 
 
